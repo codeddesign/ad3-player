4c833730e5e0e435e8292f6e5dff2ee9b668d4ed
adelin <boovad@live.com>
2017-01-10 07:32:44 +0200

    Update config example

--- example.config.js
+++ example.config.js
@@ -1,3 +1,11 @@
-export const assets = [];
+export const assets = [{
+    name: 'player',
+    tag: 'script',
+    attributes: {
+        src: `http://a3m.dev/player.js`
+    }
+}];
 
-export default {};
+export default {
+    app_path: 'http://a3m.dev'
+};
