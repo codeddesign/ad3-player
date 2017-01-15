e2cb722badd9912abfd45c93eeb8490af70ddb19
adelin <boovad@live.com>
2017-01-15 13:47:52 +0200

    Add instances
    - player
    - campaign
    - loaded tags

--- src/player/player.js
+++ src/player/player.js
@@ -1,12 +1,19 @@
+import $instance from './instance';
 import Campaign from './campaign/campaign';
 
 class Player {
     constructor(campaign, source) {
-        this.$campaign = new Campaign(campaign);
-
-        this.$campaign.requestTags().then((tags) => {
-            console.log(tags);
+        $instance.add({
+            player: this,
+            campaign: new Campaign(campaign)
         });
+
+        $instance.campaign.requestTags()
+            .then((tags) => {
+                $instance.add({ tags });
+
+                // console.log($instance);
+            });
     }
 }
 
