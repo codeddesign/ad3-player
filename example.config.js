dba90e10bcf729db9b3e850898eb9724d35553c4
adelin <boovad@live.com>
2017-01-16 13:48:43 +0200

    Add player assets - styling, images

--- example.config.js
+++ example.config.js
@@ -4,6 +4,13 @@ export const assets = [{
     attributes: {
         src: `http://a3m.dev/player.js`
     }
+}, {
+    name: 'styling',
+    tag: 'link',
+    attributes: {
+        rel: 'stylesheet',
+        href: `https://a3m.dev/style.css`
+    }
 }];
 
 export default {
