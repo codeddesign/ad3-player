8f36e6cd597b0ca561493ca699b7989a3c66981a
adelin <boovad@live.com>
2017-08-18 22:01:01 +0300

    Remove LZMA

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,6 @@ import commonjs from 'rollup-plugin-commonjs';
 import uglify from 'rollup-plugin-uglify';
 import postcss from 'rollup-plugin-postcss';
 import cssnano from 'cssnano';
-import string from 'rollup-plugin-string';
 
 /**
  * Env defaults
@@ -26,10 +25,6 @@ const plugins = [
     //     presets: ['es2015-rollup'],
     //     exclude: 'node_modules/**'
     // }),
-
-    string({
-        include: '**/*.txt',
-    }),
 
     babel({
         babelrc: false,
